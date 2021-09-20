76798d40656038120ab58449f451cb7de9273b68
Nir Adler <niradler@users.noreply.github.com>
2021-09-20 13:16:46 +0300

    tests

--- tests/FetchLifecycle.test.js
+++ tests/FetchLifecycle.test.js
@@ -1,33 +1,34 @@
 const FetchLifecycle = require("../FetchLifecycle.js");
-
-const fetchLifecycle = new FetchLifecycle(
-  "https://jsonplaceholder.typicode.com",
-  { headers: { "Content-Type": "application/json" } },
-  {
-    before: [
-      function (req, res, error) {
-        console.log("before", req);
-      },
-    ],
-    success: [
-      async function (req, res) {
-        console.log("success", req);
-      },
-    ],
-    error: [
-      (req, res, error) => {
-        console.log("error", req, res, error);
-      },
-    ],
-    after: [
-      (req, res, error) => {
-        console.log("after");
-      },
-    ],
-  }
-);
+const getNewInstance = () =>
+  new FetchLifecycle(
+    "https://jsonplaceholder.typicode.com",
+    { headers: { "Content-Type": "application/json" } },
+    {
+      before: [
+        function (req, res, error) {
+          console.log("before", req);
+        },
+      ],
+      success: [
+        async function (req, res) {
+          console.log("success", req);
+        },
+      ],
+      error: [
+        (req, res, error) => {
+          console.log("error", req, res, error);
+        },
+      ],
+      after: [
+        (req, res, error) => {
+          console.log("after");
+        },
+      ],
+    }
+  );
 
 test("get todo, and convert to json object", async () => {
+  const fetchLifecycle = getNewInstance();
   fetchLifecycle.success([
     async function (req, res) {
       this.data = await res.json();
@@ -49,7 +50,27 @@ test("get todo, and convert to json object", async () => {
     });
 });
 
+test("get todo, and extract title", async () => {
+  const fetchLifecycle = getNewInstance();
+  fetchLifecycle.success([
+    async function (req, res) {
+      const json = await res.json();
+      this.data = json.title;
+    },
+  ]);
+  await fetchLifecycle
+    .request("/todos/1")
+    .then((res) => {
+      expect(res.data).toEqual("delectus aut autem");
+    })
+    .catch((error) => {
+      console.log({ error });
+      expect(error).toBe(undefined);
+    });
+});
+
 test("get todo, throw on error", async () => {
+  const fetchLifecycle = getNewInstance();
   fetchLifecycle.error([
     async function (req, res, error) {
       throw error;
